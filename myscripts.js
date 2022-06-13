c5c5a7ca58f2ac62b1d97e82ea58b3dede654f15
NewmanViktor <veljanovski.viktor@gmail.com>
2022-06-13 13:52:32 -0400

    bio

--- myscripts.js
+++ myscripts.js
@@ -5,12 +5,11 @@ $(document).ready(function(){
         if (this.hash !== "") {
           // Prevent default anchor click behavior
           event.preventDefault();
-    
-          // Store hash
+
           var hash = this.hash;
     
           // Using jQuery's animate() method to add smooth page scroll
-          // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+          // The optional number (1300) specifies the number of milliseconds it takes to scroll to the specified area
           $('html, body').animate({
             scrollTop: $(hash).offset().top
           }, 1300, function(){
@@ -18,6 +17,6 @@ $(document).ready(function(){
             // Add hash (#) to URL when done scrolling (default click behavior)
             window.location.hash = hash;
           });
-        } // End if
+        }
       });
 });
