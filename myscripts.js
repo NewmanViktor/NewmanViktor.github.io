47ab641b710af8a7d9946e7ed83f92e2930644ed
NewmanViktor <veljanovski.viktor@gmail.com>
2022-10-19 22:14:51 -0400

    final changes

--- myscripts.js
+++ myscripts.js
@@ -1,25 +1,29 @@
 $(document).ready(function(){
-    $("a").on('click', function(event) {
-
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-          // Prevent default anchor click behavior
-          event.preventDefault();
-
-          var hash = this.hash;
-    
-          // Using jQuery's animate() method to add smooth page scroll
-          // The optional number (1300) specifies the number of milliseconds it takes to scroll to the specified area
-          $('html, body').animate({
-            scrollTop: $(hash).offset().top
-          }, 1300, function(){
-    
-            // Add hash (#) to URL when done scrolling (default click behavior)
-            window.location.hash = hash;
-          });
-        }
+    $("#button").on('click', function(event) {
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+        var hash = this.hash;
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (1300) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({scrollTop: $(hash).offset().top}, 1300, function(){window.location.hash = hash;});
+      }
+    });
+    $("#scrollToTop").on('click', function(event) {
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+        var hash = this.hash;
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (1300) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({scrollTop: $(hash).offset().top}, 3500, function(){window.location.hash = hash;});
+      }
+    });
+    $(window).load(function() {
+      $("body").removeClass("preload");
       });
-      $(window).load(function() {
-        $("body").removeClass("preload");
-      });
+
 });
+
