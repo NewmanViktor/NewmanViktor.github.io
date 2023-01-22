2c61ef8e011fb4ed0877cd66c0926e8f7cfe836c
NewmanViktor <veljanovski.viktor@gmail.com>
2023-01-22 18:26:48 -0500

    improved portfolio

--- myscripts.js
+++ myscripts.js
@@ -21,6 +21,39 @@ $(document).ready(function(){
         $('html, body').animate({scrollTop: $(hash).offset().top}, 3500, function(){window.location.hash = hash;});
       }
     });
+    $("#scroll1").on('click', function(event) {
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+        var hash = this.hash;
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (1300) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({scrollTop: $(hash).offset().top}, 1500, function(){window.location.hash = hash;});
+      }
+    });
+    $("#scroll2").on('click', function(event) {
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+        var hash = this.hash;
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (1300) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({scrollTop: $(hash).offset().top}, 2000, function(){window.location.hash = hash;});
+      }
+    });
+    $("#scroll3").on('click', function(event) {
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+        var hash = this.hash;
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (1300) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({scrollTop: $(hash).offset().top}, 2500, function(){window.location.hash = hash;});
+      }
+    });
     $(window).load(function() {
       $("body").removeClass("preload");
       });
